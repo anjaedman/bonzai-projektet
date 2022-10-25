9804d8371284e2ca59ef3ff0e097a2c67bf2ecbb
anjaedman <anjaedman@hotmail.com>
2022-10-25 10:36:17 +0200

    uppstart javascript dag 1

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+let test = 10
+console.log(" testa sträng", test);
+
+
+// let test = 1;
+
+// test = 2;
+
+// console.log("Test ändring number till annat nummer: ", test);
+
+// test = false;
+
+// console.log("Test ändring till boolean: ", test);
+
+// test = "Hej";
+
+// console.log("Test ändring till en sträng: ", test);
+
+// console.log("Slutvärdet är detta:", test);
+
+
+// let myVarible = 'Hello';
+// console.log("myVariable: ", myVarible);
+// myVarible = 100;
+// console.log("myVariable: ", myVarible);
+// myVarible = 'Hello ' + 100;
+// console.log("myVariable: ", myVarible);
+// let michaela = {
+//   userName: "Michaela001",
+//   isMale: false,
+// };
+// let joakim = {
+//   userName: "Joakim001",
+//   isMale: true,
+// };
+// // Skapar en lista utifrån nuvarande värden
+// let arrayOfUsers = [michaela, joakim];
+// console.log('arrayOfUsers', arrayOfUsers);
+// // Ändrar endast userName på joakim-objektet
+// joakim.userName = "Joakim007";
+// // Skapar en ny lista med michaela-objektet (ej förändrad) joakim-objektet (förändrad)
+// let arrayOfUsers2 = [michaela, joakim];
